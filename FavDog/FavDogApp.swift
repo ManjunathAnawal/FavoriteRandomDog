8945ee39165dd0eca997ad93d8b76947349123c3
Manjunath <manjunath.anawal3@gmail.com>
2024-02-24 08:28:16 +0530

    Initial Commit

---
+++ FavDog/FavDogApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavDogApp.swift
+//  FavDog
+//
+//  Created by Manjunath Anawal on 24/02/24.
+//
+
+import SwiftUI
+
+@main
+struct FavDogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
